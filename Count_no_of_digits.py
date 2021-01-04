69929098092b6d42024824f97535dfaa95ce9294
Ajmera Pavan Kumar <ajmerapavan20@gmail.com>
2021-01-04 22:05:23 +0530

    Add files via upload

---
+++ Count_no_of_digits.py
@@ -0,0 +1,19 @@
+#"iterative method:
+def Count_Digits(n):
+    count = 0
+    while (n != 0):
+        n = n // 10
+        count = count + 1
+    return count
+
+n = 1
+print('Number of digits in number is:',Count_Digits(n),'\n')
+
+# Recursive method:
+
+def count_digit(n):
+    if (n == 0):
+        return 0
+    return 1 + count_digit(n // 10)
+
+print("No of Digits in a number: ", count_digit(123),'\n')
