3311e1716e31ee866cabd59a520776469aa3af48
Ajmera Pavan Kumar <ajmerapavan20@gmail.com>
2021-01-05 22:53:48 +0530

    Update Count_no_of_digits.py

--- Count_no_of_digits.py
+++ Count_no_of_digits.py
@@ -17,3 +17,13 @@ def count_digit(n):
     return 1 + count_digit(n // 10)
 
 print("No of Digits in a number: ", count_digit(123),'\n')
+
+# Using while loop
+def cd(x):
+    result = 0
+    while(x > 0):
+        x = x // 10
+        result = result +1
+    return result
+x = 38
+print("No of Digits in x is: ",cd(x))
